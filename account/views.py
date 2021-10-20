c1ba1f47bf5fd76fcdb54ba72085b584e2579338
wave49192 <sirawich.tu@ku.th>
2021-10-21 02:39:17 +0700

    add Login features for users

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 
 # Create your views here.
@@ -27,3 +27,25 @@ def registration_view(request):
 def logout_view(request):
     logout(request)
     return redirect('index')
+
+
+def login_view(request):
+    context = {}
+    user = request.user
+    if user.is_authenticated:
+        return redirect('index')
+
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AccountAuthenticationForm()
+    context['login_form'] = form
+    return render(request, 'account/login.html',context)
