dd7b9b83074650226fe5fb9041654c4d2f2480d9
Noppharut Kongsakdinasarn <noppharut.k@ku.th>
2021-10-23 22:48:00 +0700

    Add two test cases

--- KUIZ/tests.py
+++ KUIZ/tests.py
@@ -1,3 +1,16 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from .models import Quiz, Question, Choice
 
-# Create your tests here.
+
+class QuizModelTests(TestCase):
+    def quiz_form(self):
+        quiz_name = Quiz(quiz_topic="Calculus I", detail="Derivative and Integration")
+        self.assertEqual(quiz_name.__str__(), "Calculus I : Derivative and Integration")
+
+
+class QuestionModelTests(TestCase):
+    def question_form(self):
+        question_des = Question(question_text="What is derivative of x^3 + 2x - 5")
+        self.assertEqual(question_des.__str__(), "What is derivative of x^3 + 2x - 5")
