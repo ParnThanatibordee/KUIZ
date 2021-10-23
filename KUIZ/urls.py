bbb0211cc57faf5b20e938abfeef873f086d3488
ParnThanatibordee <parn8946@gmail.com>
2021-10-23 11:32:22 +0700

    create a function template for develop views

--- KUIZ/urls.py
+++ KUIZ/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('quiz/', views.detail, name='detail')
 ]
--- KUIZ/views.py
+++ KUIZ/views.py
@@ -3,10 +3,34 @@ from django.http import HttpResponse
 
 from KUIZ.models import Quiz
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the KUIZ index.")
 
-def detail(request):
+def index(request):
+    """Index homepage."""
     all_quiz = Quiz.objects.all()
     output = '\n'.join([q.quiz_topic for q in all_quiz])
     return HttpResponse(output)
+
+
+def exam(request):
+    """Exam view."""
+    pass
+
+
+def question(request):
+    """Question view."""
+    pass
+
+
+def answer(request):
+    """Answer for choice or type."""
+    pass
+
+
+def result(request):
+    """Result of the exam page."""
+    pass
+
+
+def feedback(request):
+    """Feedback page for discuss with teacher."""
+    pass
