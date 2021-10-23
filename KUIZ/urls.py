c08b1ca48e1107f67b36f603755140eb2eb800ec
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-10-23 15:11:11 +0700

    create view to render detail by section

--- KUIZ/urls.py
+++ KUIZ/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detail/', views.detail, name='detail'),
     path('exam/<int:pk>', views.exam, name='exam'),
+    path('detail/<int:pk>', views.detail_by_section, name='detail'),
 ]
