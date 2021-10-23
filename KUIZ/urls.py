9bf25c75cb92d6b8f4864c8ffd4b76781fa66c69
ParnThanatibordee <parn8946@gmail.com>
2021-10-23 15:10:09 +0700

    add question.html

--- KUIZ/urls.py
+++ KUIZ/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detail/', views.detail, name='detail'),
     path('exam/<int:pk>', views.exam, name='exam'),
+    path('exam/<int:pk>/question/<int:question_id>', views.question, name='question'),
 ]
