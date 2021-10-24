bb3ed04a8dda9f20059f16cd520b60c603865639
ParnThanatibordee <parn8946@gmail.com>
2021-10-24 15:14:50 +0700

    edit bug question views

--- KUIZ/views.py
+++ KUIZ/views.py
@@ -14,27 +14,35 @@ def detail(request):
     all_question = Question.objects.all()
     return render(request, 'KUIZ/detail.html', {'questions': all_question})
 
+
 def detail_by_section(request, pk):
     quiz = Quiz.objects.get(pk=pk)
     questions = quiz.question_set.all()
     return HttpResponse(questions)
 
+
 def exam(request, pk):
     """Exam view."""
     quiz = Quiz.objects.get(pk=pk)
-    return render(request, 'KUIZ/exam.html', {'quiz': quiz})
+    all_question = quiz.question_set.all()
+    # add suffle ถ้าจะ random order คำถาม
+    question1 = all_question[0]
+    return render(request, 'KUIZ/exam.html', {'quiz': quiz, 'q1': question1})
 
 
 def question(request, pk, question_id):
     """Question view."""
     quiz = Quiz.objects.get(pk=pk)
     all_question = quiz.question_set.all()
+    num_of_question = 1  # ต้องหาทาง update
     this_question = Question.objects.get(pk=question_id)
-    num_of_question = question_id
-    return render(request, 'KUIZ/question.html', {'quiz': quiz, 'questions': all_question, 'question': this_question, 'num': num_of_question})
+    next_question = Question.objects.get(pk=question_id+1)
+    all_choice = this_question.choice_set.all()
+    return render(request, 'KUIZ/question.html', {'quiz': quiz, 'question': this_question, 'next_question': next_question, 'num': num_of_question, 'choices': all_choice})
+    # ค่อยเพิ่ม if ใน question.html
 
 
-def answer(request):
+def answer(request, pk, question_id):
     """Answer for choice or type."""
     pass
 
