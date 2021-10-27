621ca881533d451c2de2c33ccee8e3c2a3a4b41d
ParnThanatibordee <parn8946@gmail.com>
2021-10-27 16:04:24 +0700

    add url of anwser and see score

--- KUIZ/urls.py
+++ KUIZ/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('exam/<int:pk>', views.exam, name='exam'),
     path('detail/<int:pk>', views.detail_by_section, name='detail'),
     path('exam/<int:pk>/question/<int:question_id>', views.question, name='question'),
+    # path('exam/<int:pk>/question/<int:question_id>/answer', views.answer, name='question'),
+    # path('exam/<int:pk>/question/score', views.score, name='score'),
 ]
