6a266ca3ba77ce38146881a7bccacef3b3f6aaab
wave49192 <sirawich.tu@ku.th>
2021-10-29 19:01:06 +0700

    create get_feedback in views.py

--- KUIZ/views.py
+++ KUIZ/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import FeedbackForm
+from KUIZ.models import Quiz, Feedback
 
-from KUIZ.models import Quiz
 
 def index(request):
     return HttpResponse("Hello, world. You're at the KUIZ index.")
 
 
 def home(request):
-    return render(request,"KUIZ/home.html",{})
+    return render(request, "KUIZ/home.html")
+
 
 def detail(request):
     all_quiz = Quiz.objects.all()
     output = '\n'.join([q.quiz_topic for q in all_quiz])
     return HttpResponse(output)
+
+
+def get_feedback(request):
+    feedback = Feedback.objects.all()
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = FeedbackForm()
+    return render(request, "KUIZ/feedback.html", {"form": form, "feedback": feedback})
 
