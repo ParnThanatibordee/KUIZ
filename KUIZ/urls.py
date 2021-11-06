da62f24f8f291dc1139037ebca39d9592f733b9b
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-06 19:32:06 +0700

    remove url of score page

--- KUIZ/urls.py
+++ KUIZ/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('detail/<str:topic>', views.detail_by_topic, name='detail_by_topic'),
     path('exam/<int:pk>/question/<int:question_id>', views.question, name='question'),
     path('exam/<int:pk>/question/<int:question_id>/answer', views.answer, name='answer'),
-    path('exam/<int:pk>/question/score', views.score, name='score'),
     path('exam/<int:pk>/question/result', views.result, name='result'),
     path('exam/<int:pk>/question/feedback', views.feedback, name='feedback'),
 ]
