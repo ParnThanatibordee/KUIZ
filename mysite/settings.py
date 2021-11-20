e73ffd5d680bdf68076a2c602544553b22a32a98
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-11-21 01:18:13 +0700

    config database setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,8 +102,8 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+dj_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(dj_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
