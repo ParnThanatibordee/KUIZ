26c5b7401eace0978102300617972fbc377bf4f7
ParnThanatibordee <parn8946@gmail.com>
2021-11-21 23:09:21 +0700

    add Test case in test Quesition model

--- KUIZ/Tests/test_model.py
+++ KUIZ/Tests/test_model.py
@@ -36,8 +36,62 @@ class QuizModelTests(TestCase):
         recent_quiz = Quiz(pub_date=time)
         self.assertIs(recent_quiz.was_published_recently(), True)
 
+    def test_is_published_with_future_question(self):
+        """
+        is_published() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_quz = Quiz(pub_date=time)
+        self.assertIs(future_quz.is_published(), False)
 
-class QuestiongModelTests(TestCase):
+    def test_is_published_with_old_question(self):
+        """
+        is_published() returns True for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_quiz = Quiz(pub_date=time)
+        self.assertIs(old_quiz.is_published(), True)
+
+    def test_is_published_with_recent_question(self):
+        """
+        is_published() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_quiz = Quiz(pub_date=time)
+        self.assertIs(recent_quiz.is_published(), True)
+
+    def test_can_vote_with_future_question(self):
+        """
+        can_vote() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_quz = Quiz(pub_date=time)
+        self.assertIs(future_quz.can_vote(), False)
+
+    def test_can_vote_with_old_question(self):
+        """
+        can_vote() returns True for questions whose pub_date and end_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_quiz = Quiz(pub_date=time, end_date=time + datetime.timedelta(days=1))
+        self.assertIs(old_quiz.can_vote(), False)
+
+    def test_can_vote_with_recent_question(self):
+        """
+        can_vote() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_quiz = Quiz(pub_date=time)
+        self.assertIs(recent_quiz.can_vote(), True)
+
+
+class QuestionModelTests(TestCase):
     def test_question_form(self):
         question_des = Question(question_text="What is derivative of x^3 + 2x - 5")
         self.assertEqual(question_des.__str__(), "What is derivative of x^3 + 2x - 5")
