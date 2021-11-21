711e325ff8c834fa4ca796d1a1f94c150804215b
Isolation666 <noppharut.k@ku.th>
2021-11-21 14:34:15 +0700

    Added E2E Test-Case with Selenium

---
+++ KUIZ/Tests/test_E2E.py
@@ -0,0 +1,46 @@
+# import unittest
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+#
+#
+# class TestSelenium(unittest.TestCase):
+#     """Tests of the Selenium E2E."""
+#
+#     def setUp(self):
+#         """Set up an browser driver before each test."""
+#         my_options = Options()
+#         my_options.headless = True
+#
+#         assert my_options.headless
+#
+#         path_to_driver = ""  # Your PATH/TO/DRIVER
+#         self.browser = webdriver.Chrome(path_to_driver, options=my_options)
+#         self.browser.implicitly_wait(10)
+#
+#     def test_access_to_KUIZ(self):
+#         """Test that a Chrome driver can get url of KUIZ"""
+#         url = "https://ku-kuiz.herokuapp.com/kuiz/"
+#         self.browser.get(url)
+#         self.assertEqual(url, self.browser.current_url)
+#         url = "https://ku-kuiz.herokuapp.com/user/login/"
+#         self.browser.get(url)
+#         self.assertEqual(url, self.browser.current_url)
+#         url = "https://ku-kuiz.herokuapp.com/kuiz/detail/chemistry"
+#         self.browser.get(url)
+#         self.assertEqual(url, self.browser.current_url)
+#
+#     def test_get_tile(self):
+#         """Test that KUIZ in html Title"""
+#         url = "https://ku-kuiz.herokuapp.com/kuiz/detail/"
+#         self.browser.get(url)
+#         self.assertIn("KUIZ", self.browser.title)
+#
+#     def test_user_require_login(self):
+#         """Test user require to login before can customize the profile"""
+#         elements = self.browser.find_elements_by_tag_name("a")
+#         url_lists = []
+#         for links in elements:
+#             pages_index = links.get_attribute('href')
+#             url_lists.append(pages_index)
+#         profile_index = "https://ku-kuiz.herokuapp.com/user/profile/" in url_lists
+#         self.assertFalse(profile_index)
