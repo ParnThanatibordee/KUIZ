838af784ab83c1d073b8e0e1c93fe451557dab4f
ParnThanatibordee <parn8946@gmail.com>
2021-11-22 01:34:27 +0700

    add test form in test model and add check_stratrgy in test answer model

--- KUIZ/Tests/test_model.py
+++ KUIZ/Tests/test_model.py
@@ -1,7 +1,8 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
-from KUIZ.models import Quiz, Question
+from account.models import Account
+from KUIZ.models import Quiz, ClassroomUser, Question, Choice, Type, Score, Answer, Feedback, Attendee
 
 
 class QuizModelTests(TestCase):
@@ -91,7 +92,161 @@ class QuizModelTests(TestCase):
         self.assertIs(recent_quiz.can_vote(), True)
 
 
+class ClassroomUserModelTests(TestCase):
+    def test_classroom_user_form(self):
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="Basic Math")
+        classroom_user = ClassroomUser(quiz=quiz, user=user)
+        self.assertEqual(classroom_user.__str__(), "DemoTester in Basic Math")
+
+
 class QuestionModelTests(TestCase):
     def test_question_form(self):
         question_des = Question(question_text="What is derivative of x^3 + 2x - 5")
         self.assertEqual(question_des.__str__(), "What is derivative of x^3 + 2x - 5")
+
+
+class ChoiceModelTests(TestCase):
+    def test_choice_form(self):
+        question = Question(question_text="1+1=?")
+        choice_text = "2"
+        choice = Choice(question=question, choice_text=choice_text)
+        self.assertEqual(choice.__str__(), choice_text)
+
+
+class TypeUserModelTests(TestCase):
+    def test_type_form(self):
+        question = Question(question_text="1+1=?")
+        choice_text = "Answer here:"
+        choice = Type(question=question, choice_text=choice_text)
+        self.assertEqual(choice.__str__(), f"answer: {choice.pk}")
+
+
+class ScoreModelTests(TestCase):
+    pass
+
+
+class AnswerModelTests(TestCase):
+    def test_type_form(self):
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="Basic Math")
+        question = Question(question_text="1+1=?", check_strategy="static")
+        answer = Answer(user=user, quiz=quiz, question=question, answer="2")
+        self.assertEqual(answer.__str__(), "2")
+
+    def test_check_answer_with_static_strategy(self):
+        """
+        check_answer(correct) with static strategy returns True
+        for answer whose has the correct answer and return False
+        for the incorrect answer.
+        """
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="English vocabulary")
+        correct_answer = "apple"
+        my_answer1 = "apple"
+        question = Question(question_text="type apple", check_strategy="static", correct=correct_answer)
+        answer1 = Answer(user=user, quiz=quiz, question=question, answer=my_answer1)
+        self.assertEqual(answer1.check_answer(question.correct), True)
+        my_answer2 = "Apple"
+        answer2 = Answer(user=user, quiz=quiz, question=question, answer=my_answer2)
+        self.assertEqual(answer2.check_answer(question.correct), False)
+
+    def test_check_answer_with_flexible_strategy(self):
+        """
+        check_answer(correct) with flexible strategy returns True
+        for answer whose has the correct answer and return False
+        for the incorrect answer.
+        """
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="English vocabulary")
+        correct_answer = "apple"
+        my_answer1 = "apple"
+        question = Question(question_text="type apple", check_strategy="flexible", correct=correct_answer)
+        answer1 = Answer(user=user, quiz=quiz, question=question, answer=my_answer1)
+        self.assertEqual(answer1.check_answer(question.correct), True)
+        my_answer2 = "Apple"
+        answer2 = Answer(user=user, quiz=quiz, question=question, answer=my_answer2)
+        self.assertEqual(answer2.check_answer(question.correct), True)
+
+    def test_check_answer_with_white_space_order_strategy(self):
+        """
+        check_answer(correct) with white_space_order strategy returns True
+        for answer whose has the correct answer and return False
+        for the incorrect answer.
+        """
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="English vocabulary")
+        correct_answer = "apple banana"
+        my_answer1 = "apple       banana"
+        question = Question(question_text="type 'apple banana'", check_strategy="white_space_order", correct=correct_answer)
+        answer1 = Answer(user=user, quiz=quiz, question=question, answer=my_answer1)
+        self.assertEqual(answer1.check_answer(question.correct), True)
+        my_answer2 = "banana apple"
+        answer2 = Answer(user=user, quiz=quiz, question=question, answer=my_answer2)
+        self.assertEqual(answer2.check_answer(question.correct), False)
+
+    def test_check_answer_with_white_space_no_order_strategy(self):
+        """
+        check_answer(correct) with white_space_no_order strategy returns True
+        for answer whose has the correct answer and return False
+        for the incorrect answer.
+        """
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="English vocabulary")
+        correct_answer = "apple banana"
+        my_answer1 = "apple       banana"
+        question = Question(question_text="type 'apple banana'", check_strategy="white_space_no_order", correct=correct_answer)
+        answer1 = Answer(user=user, quiz=quiz, question=question, answer=my_answer1)
+        self.assertEqual(answer1.check_answer(question.correct), True)
+        my_answer2 = "banana       apple"
+        answer2 = Answer(user=user, quiz=quiz, question=question, answer=my_answer2)
+        self.assertEqual(answer2.check_answer(question.correct), True)
+
+    def test_check_answer_with_comma_order_strategy(self):
+        """
+        check_answer(correct) with comma_order strategy returns True
+        for answer whose has the correct answer and return False
+        for the incorrect answer.
+        """
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="English vocabulary")
+        correct_answer = "apple, banana"
+        my_answer1 = "apple, banana"
+        question = Question(question_text="type 'apple banana'", check_strategy="comma_order", correct=correct_answer)
+        answer1 = Answer(user=user, quiz=quiz, question=question, answer=my_answer1)
+        self.assertEqual(answer1.check_answer(question.correct), True)
+        my_answer2 = "banana, apple"
+        answer2 = Answer(user=user, quiz=quiz, question=question, answer=my_answer2)
+        self.assertEqual(answer2.check_answer(question.correct), False)
+
+    def test_check_answer_with_comma_no_order_strategy(self):
+        """
+        check_answer(correct) with comma_no_order strategy returns True
+        for answer whose has the correct answer and return False
+        for the incorrect answer.
+        """
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="English vocabulary")
+        correct_answer = "apple, banana"
+        my_answer1 = "apple, banana"
+        question = Question(question_text="type 'apple banana'", check_strategy="comma_no_order", correct=correct_answer)
+        answer1 = Answer(user=user, quiz=quiz, question=question, answer=my_answer1)
+        self.assertEqual(answer1.check_answer(question.correct), True)
+        my_answer2 = "banana, apple"
+        answer2 = Answer(user=user, quiz=quiz, question=question, answer=my_answer2)
+        self.assertEqual(answer2.check_answer(question.correct), True)
+
+
+class FeedbackModelTests(TestCase):
+    def test_classroom_user_form(self):
+        user = Account(username="DemoTester")
+        quiz = Quiz(quiz_topic="Basic Math")
+        feedback_text = "Good job"
+        feedback = Feedback(user=user, quiz=quiz, feedback_text=feedback_text)
+        self.assertEqual(feedback.quiz_name(), "Basic Math")
+        self.assertEqual(feedback.username(), "DemoTester")
+        self.assertEqual(feedback.__str__(), "Good job")
+
+
+class AttendeeModelTests(TestCase):
+    pass
