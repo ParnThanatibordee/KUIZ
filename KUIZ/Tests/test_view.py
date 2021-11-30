87e37470e0d24f3fd3c4a3b9d3642c088ccf0f7d
Noppharut Kongsakdinasarn <78093958+Isolation666@users.noreply.github.com>
2021-11-30 15:08:17 +0700

    Update test_view.py

--- KUIZ/Tests/test_view.py
+++ KUIZ/Tests/test_view.py
@@ -70,3 +70,28 @@ class QuizIndexViewTests(TestCase):
         self.assertQuerysetEqual(
             response.context['latest_quiz_list'],
             [quiz2, quiz1],)
+        
+class QuestionDetailViewTests(TestCase):
+    """A class for checking detail view."""
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='username', email='testemail@mail.com', password='147896325')
+        # self.user.first_name = "testuser"
+        # self.user.last_name = "hello"
+        # self.user.save()
+
+    def test_future_question(self):
+        """The detail view of a question with a pub_date in the future returns to index page."""
+        self.client.login(username='username', password='qwerxhucj12')
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_past_question(self):
+        """The detail view of a question with a pub_date in the past displays the question's text."""
+        self.client.login(username='username', password='qwerxhucj12')
+        past_question = create_question(question_text="Past question.", days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
